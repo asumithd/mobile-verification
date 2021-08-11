cdea8e1a9e471a077479e069e39d6d2ceec53ffa
asumithd <sumithdeepan@gmail.com>
2021-08-12 00:06:31 +0530

    mobile verufication

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  countryList: any[] = [];
+  mobileForm: FormGroup;
+  valid: Boolean;
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.mobileForm = this.formBuilder.group({
+      codeNo: [''],
+      phoneNo: [''],
+    });
+  }
+  onSubmit() {
+    const data = this.mobileForm.value;
+    const access_key = 'a1eae114356eb7c743c42f4c8091eb35';
+    const country_code = data.codeNo;
+    const phone_number = data.phoneNo;
+
+    this.http
+      .get(
+        `http://apilayer.net/api/validate?access_key=${access_key}&number=${phone_number}&country_code=${country_code}&format=1`
+      )
+      .subscribe((data) => {
+        this.countryList = [...this.countryList, data];
+        this.valid = this.countryList[0].valid;
+        console.log(this.countryList, 'data');
+      });
+    this.mobileForm.reset();
+  }
+  onOptionsSelected() {
+    console.log('onOptionsSelected');
+    this.countryList = [];
+  }
+}
