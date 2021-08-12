7ffb036d72602ee202dc76de96edb5a79d38c894
asumithd <sumithdeepan@gmail.com>
2021-08-12 21:54:04 +0530

    mobile verfication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   countryList: any[] = [];
   mobileForm: FormGroup;
   valid: Boolean;
+  link: any;
   constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -24,19 +25,35 @@ export class AppComponent implements OnInit {
     const country_code = data.codeNo;
     const phone_number = data.phoneNo;
 
+    const httpLink = `http://apilayer.net/api/validate?access_key=${access_key}&number=${phone_number}&country_code=${country_code}&format=1`;
+    const body = {
+      dynamicLinkInfo: {
+        domainUriPrefix: 'https://asetapp.page.link',
+        link: `${httpLink}`,
+      },
+      suffix: {
+        option: 'SHORT',
+      },
+    };
+
     this.http
-      .get(
-        `http://apilayer.net/api/validate?access_key=${access_key}&number=${phone_number}&country_code=${country_code}&format=1`
+      .post(
+        `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyCDeZvGA43xELvKaGa7eLIcqhcRvjQdWak`,
+        body
       )
-      .subscribe((data) => {
-        this.countryList = [...this.countryList, data];
-        this.valid = this.countryList[0].valid;
-        console.log(this.countryList, 'data');
+      .subscribe((res: any) => {
+        this.link = res.shortLink;
+        console.log('>>>>', this.link);
+        this.http.get(this.link).subscribe((data) => {
+          this.countryList = [...this.countryList, data];
+          this.valid = this.countryList[0].valid;
+          console.log(this.countryList, 'data');
+        });
       });
+
     this.mobileForm.reset();
   }
   onOptionsSelected() {
-    console.log('onOptionsSelected');
     this.countryList = [];
   }
 }
